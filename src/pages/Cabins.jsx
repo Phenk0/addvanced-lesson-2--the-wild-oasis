d77c3886b0c118cdcbf687d8ec5ac3b7dbc7d5c7
Roman Parkhomenko <roman.s.parkhomenko@gmail.com>
2023-08-18 16:44:27 +0300

    end supabase crash lesson

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,21 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+import { getCabins } from '../services/apiCabins.js';
+import { useEffect, useState } from 'react';
 
 function Cabins() {
+  const [cabins, setCabins] = useState([]);
+
+  useEffect(() => {
+    getCabins().then((data) => setCabins(data));
+  }, []);
+
+  console.log(cabins);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src={cabins[0]?.image} alt="01" />
     </Row>
   );
 }
